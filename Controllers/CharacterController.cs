7eeeb8bf2e8285652e8700900ec3d44bf6be02d9
biswa1609 <biswajit2020@gmail.com>
2021-08-18 15:51:11 +0530

    Http post

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Core_API.Model;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,11 +15,23 @@ namespace Core_API.Controllers
         }
         public List<character> characters = new List<character>{
                 new character(),
-                new character{Name = "Sam"}
+                new character{Name = "Sam"},
+                new character{Id = 1}
             };
         [HttpGet("GetAll")]
         public ActionResult<List<character>> Get()
         {
+            return Ok(characters);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<List<character>> Get(int id)
+        {
+            return Ok(characters.Where(x=>x.Id == id).FirstOrDefault());
+        }
+        [HttpPost("Addcharecter")]
+        public ActionResult<List<character>> Addcharecter(character cha)
+        {
+            characters.Add(cha);
             return Ok(characters);
         }
 
